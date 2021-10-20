68a2667924add7606edd86445081f9f175f2d8e4
Brianna Chisholm <70978684+BriSC60962@users.noreply.github.com>
2021-10-20 15:38:51 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -14,32 +14,127 @@ namespace Health_System_v1._0
         static int damage;
         static string healthStatus;
         static int spillover;
+        static int exp = 0;
+        static int level = 1;
+        static int score;
+        static int hp;
 
         static void Main(string[] args)
         {
             Console.WriteLine("Health System");
+            Console.WriteLine("By Brianna Chisholm");
+            Console.WriteLine("///////////////////////");
+            Console.ReadKey(true);
+
+            Console.WriteLine();
+            ShowHUD();
+            Console.ReadKey(true);
+            
+            Console.WriteLine();
+            damage = 40;
+            TakeDamage(damage);
             ShowHUD();
             Console.WriteLine();
             Console.ReadKey(true);
-            damage = 101;
-            TakeDamage(damage);
-            ShowHUD();
-            Console.ReadKey(true);
+
+            Console.WriteLine("Resetting...");
+            Reset();
             Console.WriteLine();
-            damage = 100;
-            TakeDamage(damage);
             ShowHUD();
-            TakeDamage(damage);
-            ShowHUD();
-            TakeDamage(damage);
-            ShowHUD();
-            TakeDamage(damage);
-            ShowHUD();
-            TakeDamage(damage);
-            ShowHUD();
-            TakeDamage(damage);
-            ShowHUD();
+            Console.WriteLine();
             Console.ReadKey(true);
+
+            Console.WriteLine();
+            damage = 125;
+            TakeDamage(damage);
+            ShowHUD();
+            Console.WriteLine();
+            Console.ReadKey(true);
+
+            Console.WriteLine("Resetting...");
+            Reset();
+            Console.WriteLine();
+            ShowHUD();
+            Console.WriteLine();
+            Console.ReadKey(true);
+
+            Console.WriteLine();
+            damage = 250;
+            TakeDamage(damage);
+            ShowHUD();
+            Console.WriteLine();
+            Console.ReadKey(true);
+
+            Console.WriteLine("Resetting...");
+            Reset();
+            Console.WriteLine();
+            ShowHUD();
+            Console.WriteLine();
+            Console.ReadKey(true);
+
+            Console.WriteLine();
+            damage = -10;
+            TakeDamage(damage);
+            ShowHUD();
+            Console.WriteLine();
+            Console.ReadKey(true);
+
+            Console.WriteLine("Resetting...");
+            Reset();
+            Console.WriteLine();
+            ShowHUD();
+            Console.WriteLine();
+            Console.ReadKey(true);
+
+            Console.WriteLine();
+            hp = 100;
+            Heal(hp);
+            ShowHUD();
+            Console.WriteLine();
+            Console.ReadKey(true);
+
+            Console.WriteLine("Resetting...");
+            Reset();
+            Console.WriteLine();
+            ShowHUD();
+            Console.WriteLine();
+            Console.ReadKey(true);
+
+            Console.WriteLine();
+            damage = 150;
+            TakeDamage(damage);
+            ShowHUD();
+            Console.WriteLine();
+            Console.ReadKey(true);
+
+            Console.WriteLine();
+            hp = 100;
+            Heal(hp);
+            ShowHUD();
+            Console.WriteLine();
+            Console.ReadKey(true);
+
+            Console.WriteLine("Resetting...");
+            Reset();
+            Console.WriteLine();
+            ShowHUD();
+            Console.WriteLine();
+            Console.ReadKey(true);
+
+            Console.WriteLine();
+            hp = -100;
+            Heal(hp);
+            ShowHUD();
+            Console.WriteLine();
+            Console.ReadKey(true);
+
+            Console.WriteLine("Resetting...");
+            Reset();
+            Console.WriteLine();
+            ShowHUD();
+            Console.WriteLine();
+            Console.ReadKey(true);
+
         }
         static void ShowHUD()
         {
@@ -59,6 +154,15 @@ namespace Health_System_v1._0
             {
                 health = 100;
             }
+            if (lives < 0)
+            {
+                lives = 0;
+            }
+            if (lives > 99)
+            {
+                lives = 99;
+            }
+
 
 
             if (health == 100)
@@ -88,33 +192,58 @@ namespace Health_System_v1._0
             }
 
 
-
-
-
+            Console.WriteLine("/////////////////////////////////");
+            Console.WriteLine("Level: " + level);
+            Console.WriteLine("Player Exp: " + exp);
             Console.WriteLine("Health: " + health + "%");
             Console.WriteLine("Health Status: " + healthStatus);
             Console.WriteLine("Shield: " + shield);
             Console.WriteLine("Lives: " + lives);
+            Console.WriteLine("/////////////////////////////////");
             Console.WriteLine();
 
 
 
         }
+        static void Experience(int score)
+        {
+            exp = exp + score;
 
+            if (exp == 100)
+            {
+                Console.WriteLine("Level Up!");
+                level = level + 1;
+            }
+        }
 
         static void TakeDamage(int damage)
         {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("DEBUG: Player is about to take " + damage + " hit points of damage...");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+            if(damage < 0)
+            {
+                Console.WriteLine("ERROR: Cannot take Damage of a negative value, must be a positive value");
+                damage = 0;
+            }
             if (damage > shield)
             {
                 spillover = damage - shield;
                 health = health - spillover;
                 shield = shield - damage;
             }
-            if (health == 0)
+            if (health <= 0)
             {
+                Console.WriteLine("Player lost a life.");
+                Console.WriteLine("Player used a 1UP and restored their Health and Shield!");
+                Console.WriteLine();
                 lives = lives - 1;
                 health = 100;
                 shield = 100;
+
+
             }
 
             else if (shield > 0)
@@ -124,14 +253,6 @@ namespace Health_System_v1._0
             }
 
 
-            if (health <= 0)
-            {
-                lives = lives - 1;
-                health = 100;
-                shield = 100;
-
-
-            }
 
 
 
@@ -164,18 +285,39 @@ namespace Health_System_v1._0
 
 
         }
+        static void Reset()
+        {
+            exp = 100;
+            level = 1;
+            shield = 100;
+            health = 100;
+            lives = 3;
+
+        }
 
         static void Heal(int hp)
         {
-
-
+            Console.WriteLine("DEBUG: Player is about to Heal by " + hp + " hit points");
+            Console.WriteLine();
+            if (hp < 0)
+            {
+                Console.WriteLine("ERROR: Cannot Heal Player by a negative number, must be a positive number");
+                Console.WriteLine();
+                hp = 0;
+            }
             health = health + hp;
         }
 
         static void RegenerateShield(int hp)
         {
-
-
+            Console.WriteLine("DEBUG: Player is about to Regenerate Shield by " + hp + " hit points");
+            Console.WriteLine();
+            if (hp < 0)
+            {
+                Console.WriteLine("ERROR: Cannot Regenerate Shield by a negative number, must be a positive number");
+                Console.WriteLine();
+                hp = 0;
+            }
             shield = shield + hp;
         }
     }
